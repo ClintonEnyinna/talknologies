37f4dfb15a852c9ae58f78071b5a84db8639df7f
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-24 12:16:14 -0600

    add index and uniqness to username column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_181215) do
+ActiveRecord::Schema.define(version: 2020_03_24_181453) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_181215) do
     t.string "coverimage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
