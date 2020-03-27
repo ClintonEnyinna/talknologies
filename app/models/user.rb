27dca2d83a2f407187b78f387b0de0d53ceba493
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-26 23:22:24 -0600

    add default image for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ApplicationRecord
+  before_create :image_nil
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :fullname, presence: true
 
-  has_many :opinions, foreign_key: 'author_id'
+  has_many :opinions, foreign_key: 'author_id', dependent: :destroy
 
   has_many :followed_users, foreign_key: :follower_id, class_name: "Following"
   has_many :followees, through: :followed_users
 
   has_many :following_users, foreign_key: :followed_id, class_name: "Following"
   has_many :followers, through: :following_users
+
+  has_one_attached :image
+
+  def image_nil
+    if !self.image.attached?
+      self.image.attach(io: File.open('app/assets/images/default.jpeg'), filename: 'default.jpeg', content_type: 'image/jpeg')
+    end
+  end
 end
