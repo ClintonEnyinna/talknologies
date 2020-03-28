74e28c2876661adc8db56caa5a6897317befc54c
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-27 22:41:21 -0600

    test: model association

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -6,11 +6,6 @@ RSpec.feature "Tweets", type: :feature do
                 username: 'user1')
   end
 
-  def submit
-    form = find("form")
-    Capybara::RackTest::Form.new(page.driver, form.native).submit({})
-  end
-
   scenario 'User makes a tweet' do
     log_in(user1)
     fill_in 'opinion_text', with: "This is a test"
@@ -22,6 +17,11 @@ RSpec.feature "Tweets", type: :feature do
     visit login_path
     fill_in 'Username', with: user.username
     click_button 'Log in'
+  end
+
+  def submit
+    form = find("form")
+    Capybara::RackTest::Form.new(page.driver, form.native).submit({})
   end
 end
 
---
+++ spec/models/following_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Following, type: :model do
+  describe 'friendship' do
+    let(:user1) do
+      User.create(fullname: 'user one',
+                  username: 'user1')
+    end
+    let(:user2) do
+      User.create(fullname: 'user two',
+                  username: 'user2')
+    end
+
+    let(:follow) { Following.create(follower_id: user1.id, followed_id: user2.id) }
+
+    it 'user has follower' do
+      expect(user2.followers.first).to eq(User.find_by(username: "user1"))
+    end
+
+    it 'user follows' do
+      expect(user1.followees.first).to eq(User.find_by(username: "user2"))
+    end
+  end
+end
