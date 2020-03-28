3e36eee35a7a9df95e7f5ee94774733112f250dd
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-27 20:59:19 -0600

    created leftnav and tweet partials to refactor users and opions views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @opinions = @user.opinions
+    @opinions = @user.opinions.includes(:author)
   end
 
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def current_user?(user)
     user == current_user
   end
+
+  def followers(user)
+    user.followers
+  end
 end
