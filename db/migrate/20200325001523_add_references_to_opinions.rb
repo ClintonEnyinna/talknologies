85c4a3a1d03db7cf70b407d6ede70365930de3d3
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-27 19:33:50 -0600

    correct migration code

--- db/migrate/20200325001523_add_references_to_opinions.rb
+++ db/migrate/20200325001523_add_references_to_opinions.rb
@@ -1,5 +1,5 @@
 class AddReferencesToOpinions < ActiveRecord::Migration[5.2]
   def change
-    add_reference :opinions, :author, references: :user, index: true, foreign_key: true
+    add_reference :opinions, :author, foreign_key: { to_table: :users }, index: true
   end
 end
