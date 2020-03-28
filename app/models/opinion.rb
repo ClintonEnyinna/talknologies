24d6c29a51219d78d8379540c9457b5fb3ecefd2
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-27 19:36:43 -0600

    fix belongs_to relationship in opinion

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,5 +1,5 @@
 class Opinion < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
