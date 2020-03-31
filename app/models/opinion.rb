3c77181eaa32769ae329beda46057266eca45bb6
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-31 12:21:05 -0600

    add presence validation to opinion

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,5 +1,7 @@
 class Opinion < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
+  validates :text, presence: true
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
