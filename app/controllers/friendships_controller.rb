732d5a28a8aed623f2fe231aa053151474107d10
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-04-14 23:28:28 -0500

    refactor: avoid page reload when following or unfollowing

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,12 +2,12 @@ class FriendshipsController < ApplicationController
   def create
     @friend = User.find(params[:id])
     follow(@friend)
-    redirect_to @friend
+    redirect_to request.referrer
   end
 
   def destroy
     @friend = User.find(params[:id])
     unfollow(@friend)
-    redirect_to @friend
+    redirect_to request.referrer
   end
 end
