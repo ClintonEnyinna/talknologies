fa3a3d59c488bc84d1a550f13be33b493ee49b65
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-04-15 21:12:19 -0500

    feat: use ajax to update for like and dislike actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(opinion_id: params[:opinion_id], user: current_user)
     @like.save
-    redirect_to request.referrer
   end
 
   def destroy
     @like = Like.find_by(id: params[:id], user: current_user, opinion_id: params[:opinion_id])
     @like&.destroy
-    redirect_to request.referrer
   end
 end
