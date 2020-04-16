2fb6c600fcc5cb2b24d2abbbb9c8b744039edd94
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-04-15 22:22:15 -0500

    feat: user ajax to follow and unfollow users

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,12 +2,10 @@ class FriendshipsController < ApplicationController
   def create
     @friend = User.find(params[:id])
     follow(@friend)
-    redirect_to request.referrer
   end
 
   def destroy
     @friend = User.find(params[:id])
     unfollow(@friend)
-    redirect_to request.referrer
   end
 end
